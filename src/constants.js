517150396eb65603e9ba6d140e1892d2a8a9b91e
Gustavo Monteiro <gustavo.monteiro@stone.com.br>
2022-11-10 13:30:35 -0300

    feat: add scripts to merge pull requests as GitHub action.

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+const deployBranchName = 'sdx-prs-deploy'
+const deployRefName = `heads/${deployBranchName}`
+const deployRefHead = `refs/${deployRefName}`
+
+const token = process.env.GITHUB_TOKEN
+const sdx = process.env.SDX_BRANCH_NAME || 'sdx'
+const ref = `heads/${sdx}`
+
+const repoInfo = {
+    owner: process.env.OWNER,
+    repo: process.env.REPO,
+}
+
+export {
+    deployBranchName,
+    deployRefName,
+    deployRefHead,
+    repoInfo,
+    token,
+    ref,
+}
